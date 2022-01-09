eb22db723e413055676a9d68bddba79b6ec0337f
geekwen <geekwen@tencent.com>
2022-01-09 23:45:15 +0800

    feat: 增加备案号信息

--- src/components/LayoutFooter.vue
+++ src/components/LayoutFooter.vue
@@ -2,7 +2,13 @@
   <footer class="cf-layout-footer">
     <div class="cf-layout-footer-content">
       <div>Copyright © {{startYear}} - {{curYear}} cfweb. All Rights Reserved</div>
-      <div>Cfweb 版权所有</div>
+      <div>Cfweb 版权所有
+        <p>
+          备案号: <a href="https://beian.miit.gov.cn/">粤ICP备2021031984号</a>
+          &nbsp;&nbsp;&nbsp;&nbsp;<img src="http://www.beian.gov.cn/img/new/gongan.png">
+          公安备案号: <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030902002737">44030902002737号</a>
+        </p>
+      </div>
     </div>
   </footer>
 </template>
